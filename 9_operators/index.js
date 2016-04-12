cfb839bde0ec9963c4923717d73ffa4510031a3e
Arche Galla <artempushkar@gmail.com>
2016-04-12 15:08:44 +0300

    add idea to git ignore

--- 9_operators/index.js
+++ 9_operators/index.js
@@ -2,9 +2,9 @@ var number = 2 + 4;
 
 console.log(number);
 
-function add(a, b) {
-  return // add two numbers
-}
+// function add(a, b) {
+//   return // add two numbers
+// }
 
 
 //console.log(3 + 4 * 5 /* возвращает 23*/);
