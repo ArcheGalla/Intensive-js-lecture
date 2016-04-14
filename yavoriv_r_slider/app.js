ddd69025541fc7d498334d6551f8caefb4c30af2
Arche Galla <artempushkar@gmail.com>
2016-04-14 10:57:29 +0300

    yavoriv  - input is single tag  - format of code  - operator && instead of &  - unused param

--- yavoriv_r_slider/app.js
+++ yavoriv_r_slider/app.js
@@ -1,64 +1,65 @@
 var images = document.getElementsByTagName('img');
-var prev   = document.getElementById('prev');
-var next   = document.getElementById('next');
-var form   = document.getElementById("set_interval");
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+var form = document.getElementById("set_interval");
 
 var index = 2;
 var intervalTime = 5000;
 
 images[index].style.display = 'block';
 
-function slider(direction, interval) {
-	images[index].style.display = 'none';
+function slider(direction) {
+  images[index].style.display = 'none';
 
-	if (index === 0 & direction === -1) {
-		index = images.length - 1;
-	}
+  if (index === 0 && direction === -1) {
+    index = images.length - 1;
+  }
 
-	if (index === images.length-1 & direction === 1) {
-		index = 0;
-	}
+  if (index === images.length - 1 && direction === 1) {
+    index = 0;
+  }
 
-	index += direction;
-	images[index].style.display = 'block';
+  index += direction;
+  images[index].style.display = 'block';
 }
 
-next.addEventListener('click', function() {
-	slider(1);
-})
+next.addEventListener('click', function () {
+  slider(1);
+});
 
-prev.addEventListener('click', function() {
-	slider(-1);
-})
+prev.addEventListener('click', function () {
+  slider(-1);
+});
 
-var intervalFunc = setInterval(function() {
-	if(index === 0) {
-		slider(1)
-	} else if (index === images.length-1) {
-		slider(-1)
-	} else {
-		Math.random() > 0.5 ? slider(1) : slider(-1);
-	}		
+var intervalFunc = setInterval(function () {
+  if (index === 0) {
+    slider(1)
+  } else if (index === images.length - 1) {
+    slider(-1)
+  } else {
+    Math.random() > 0.5 ? slider(1) : slider(-1);
+  }
 }, intervalTime);
 
-form.addEventListener('submit', function(e){
-	e.preventDefault();
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-	clearInterval(intervalFunc);
+  clearInterval(intervalFunc);
 
-	var n = document.getElementById("intervar_number").value;
+  var n = document.getElementById("intervar_number").value;
   intervalTime = Number(n);
-  
-  intervalFunc = setInterval(function() {
-		if(index === 0) {
-			slider(1)
-		} else if (index === images.length-1) {
-			slider(-1)
-		} else {
-			Math.random() > 0.5 ? slider(1) : slider(-1);
-		}
-	}, intervalTime);
-})
+
+  intervalFunc = setInterval(function () {
+    if (index === 0) {
+      slider(1)
+    } else if (index === images.length - 1) {
+      slider(-1)
+    } else {
+      Math.random() > 0.5 ? slider(1) : slider(-1);
+    }
+  }, intervalTime);
+
+});
 
 
 
